50faa685d0f9c8f5dce3fb2a8c18946cf628bc5b
jombi <jombi@duck.com>
2025-01-20 05:49:12 +0900

    feat :: Pronoun Transformer - utils

--- src/commonMain/kotlin/dev/jombi/hangulkt/hangul/HangulChar.kt
+++ src/commonMain/kotlin/dev/jombi/hangulkt/hangul/HangulChar.kt
@@ -1,3 +1,9 @@
 package dev.jombi.hangulkt.hangul
 
-public data class HangulChar(val choseong: Char, val jungseong: String, val jongseong: String = "")
+public data class HangulChar(val choseong: Char, val jungseong: String, val jongseong: String = "") {
+    public fun replace받침ㅎ(): HangulChar {
+        return copy(
+            jongseong = jongseong.replace("ㅎ", "")
+        )
+    }
+}
